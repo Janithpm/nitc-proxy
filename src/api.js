74039758d6284c202af714acfed1f51d1d67059b
Janith Madarasinghe <janithpm9991@gmail.com>
2023-07-22 00:45:10 +0530

    proxy created

---
+++ src/api.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const dotenv = require("dotenv");
+const axios = require('axios')
+
+dotenv.config();
+
+const app = express();
+const router = express.Router();
+
+router.get("/health", (req, res) => {
+  res.send("I'm alive!");
+});
+
+router.post('/confirm', (req, res) => {
+  const csrfToken = req.body.reqid
+  const clientRef = req.body.clientRef
+  const url = `https://sampath.paycorp.lk/webinterface/qw/confirm?csrfToken=${csrfToken}&authToken=${process.env.PG_AUTH_TOKEN}&clientRef=${clientRef}`
+  axios.post(url, {})
+      .then((response) => {
+          const responseString = response.data
+          const responseArray = responseString.split('&')
+          const responseObj = {}
+          responseArray.forEach((item) => {
+              const [key, value] = item.split('=')
+              responseObj[key] = value
+          })
+
+          console.log(responseObj)
+          res.status(200).send(responseObj)
+      })
+      .catch((error) => {
+          console.log(error)
+          res.status(500).send(error)
+      })
+
+}
+)
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
